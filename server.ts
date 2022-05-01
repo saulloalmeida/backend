25632bbcca814435f323a0cb37a3c19e5337c134
Saullo <saullorhamon@gmail.com>
2022-04-30 23:24:50 -0300

    update const port for production

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ import express from 'express'
 import { router } from './routes'
 
 const app = express()
-// const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 3333
 app.use(cors())
 app.use(express.json())
 app.use(router)
-app.listen(3333, () => { console.log('Server started on port 3333') })
+app.listen(PORT, () => { console.log('Server started on port 3333') })
